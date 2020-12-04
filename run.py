2375614d4e04c4e0c6006f56e692732fc80f31ae
george-cw <645090758@qq.com>
2020-12-04 11:00:47 +0800

    add mqtt

--- run.py
+++ run.py
@@ -5,9 +5,26 @@ from flask import Flask, redirect, url_for, request
 from flask import jsonify, abort
 import sys
 import logging
+import paho.mqtt.client as mqtt
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
+
+def on_connect(client, userdata, flags, rc):
+    app.logger.error("Connected with result code "+str(rc))
+
+def on_publish(client, userdata, mid):
+    app.logger.error('on_publish: %s', str(mid))
+
+def on_disconnect(client, userdata, rc):
+    app.logger.error('on_disconnect: %s', str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.on_disconnect = on_disconnect
+client.connect_async("storybox.athenamuses.cn", 1884, 60)
+client.loop_start()
 
 def log(*args):
   print(args[0] % (len(args) > 1 and args[1:] or []))
@@ -39,6 +56,7 @@ def index():
       dict1 = json.loads(a)
       #log('recv: %s' % a)
       app.logger.error('recv: %s', a)
+      client.publish("xiaobaoaudio", "play", 1)
 
       return redirect(url_for('hello_world'))#json.dumps(dict1["data"])
   else:
